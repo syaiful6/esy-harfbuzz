d4525acf8407054ba0e35eea8f38fd40124ca637
syaiful6 <syai.bahri6@gmail.com>
2025-08-09 14:33:55 +0700

    build shared library in Linux

--- esy/configure-linux.sh
+++ esy/configure-linux.sh
@@ -1,3 +1,3 @@
 cd _build/build
 
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$cur__install
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$cur__install
